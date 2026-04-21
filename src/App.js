98ab3bd782d43526747558b902f9c0ed83464606
Feyruz Alizada <firuz0643@gmail.com>
2026-04-21 17:26:38 +0400

    added performanse tracker with use Ref

--- src/App.js
+++ src/App.js
@@ -3,5 +3,16 @@ import MyErrorBoundary from './components/MyErrorBoundary';
 import TransactionModule from './components/TransactionModule';
 
 function App() {
+  // FEYRUZ YAZIB COMMENTI, OZUM YAZMISAM!
+  //performansi(MS yoxluyuruq)
+  const startTime = React.useRef(performance.now());
   
+  React.useEffect(() => {
+    const duration = performance.now() - startTime;
+    console.log(`⏱️Rendered for ${duration.toFixed(2)} ms`);
+  }, []);
+
+ 
 }
+
+export default App;
